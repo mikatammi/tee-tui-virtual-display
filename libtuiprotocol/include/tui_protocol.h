8076aaa2844886e1aadbeb9ba7dbe9ae959952b8
Mika Tammi <mikatammi@gmail.com>
2014-12-25 21:26:09 +0200

    Clean away code from TUI protocol serialization
    Signed-off-by: Mika Tammi <mikatammi@gmail.com>

--- libtuiprotocol/include/tui_protocol.h
+++ libtuiprotocol/include/tui_protocol.h
@@ -24,51 +24,16 @@ extern "C" {
 #include "tui_datatypes.h"
 #include <stdbool.h>
 #include <stdint.h>
-#include <stdio.h>
 #include <stdlib.h>
 
 #define TUI_MSG_HEADER_SIZE (2 * sizeof(uint32_t))
 
-enum {
-	TUI_MSGTYPE_CHECKTEXTFORMATREQUEST = 0x01,
-	TUI_MSGTYPE_CHECKTEXTFORMATRESPONSE = 0x02,
-	TUI_MSGTYPE_GETSCREENINFOREQUEST = 0x03,
-	TUI_MSGTYPE_GETSCREENINFORESPONSE = 0x04,
-	TUI_MSGTYPE_INITSESSIONREQUEST = 0x05,
-	TUI_MSGTYPE_INITSESSIONRESPONSE = 0x06,
-	TUI_MSGTYPE_CLOSESESSIONREQUEST = 0x07,
-	TUI_MSGTYPE_CLOSESESSIONRESPONSE = 0x08,
-	TUI_MSGTYPE_DISPLAYSCREENREQEUST = 0x09,
-	TUI_MSGTYPE_DISPLAYSCREENRESPONSE = 0x0A
+enum tui_msg_type {
+	TUI_MSGTYPE_CHECKTEXTFORMAT = 1,
+	TUI_MSGTYPE_GETSCREENINFO,
+	TUI_MSGTYPE_INITSESSION,
+	TUI_MSGTYPE_CLOSESESSION,
+	TUI_MSGTYPE_DISPLAYSCREEN
 };
-
-struct checktextformatrequest {
-	size_t text_length;
-	char *text;
-};
-
-struct checktextformatresponse {
-	uint32_t ret;
-	uint32_t width;
-	uint32_t height;
-	uint32_t last_index;
-};
-
-struct getscreeninforequest {
-	uint32_t orientation;
-	uint32_t entryfield_count;
-};
-
-uint32_t calc_checktextformatrequest_size(struct checktextformatrequest *s);
-
-uint32_t generate_checktextformatrequest(FILE *stream,
-					 struct checktextformatrequest *s);
-
-uint32_t generate_checktextformatresponse(FILE *stream,
-		                          struct checktextformatresponse *s);
-
-#ifdef __cplusplus
-}
-#endif /* __cplusplus */
 
 #endif /* __TUI_PROTOCOL_H__ */
